32aed8f486158a4a0693500adf2b28682c9c5853
Shandy Siswandi <shandysiswandi@gmail.com>
2020-11-24 01:51:20 +0700

    add check env APP_MODE

--- app/env.go
+++ app/env.go
@@ -2,12 +2,19 @@ package app
 
 import (
 	"log"
+	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func env() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal(err.Error())
+	}
+
+	// check if development or production
+	if os.Getenv("APP_MODE") == "production" {
+		gin.SetMode(gin.ReleaseMode)
 	}
 }
