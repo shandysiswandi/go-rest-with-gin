748fc336e9c8efdf3327511ca386b9697ae8ae02
Shandy Siswandi <shandysiswandi@gmail.com>
2020-11-24 01:52:28 +0700

    update user service

--- services/user.go
+++ services/user.go
@@ -1,6 +1,33 @@
 package services
 
-// GetUser is ..
-func GetUser() {
-	//
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/shandysiswandi/go-rest-with-gin/models"
+	"github.com/shandysiswandi/go-rest-with-gin/repositories"
+	"github.com/shandysiswandi/go-rest-with-gin/utils"
+)
+
+// UserService is .
+type UserService interface {
+	GetAll(c *gin.Context)
+}
+
+// userService is private struct
+type userService struct {
+	users []models.User
+}
+
+// NewUser is contstructor
+func NewUser() UserService {
+	return &userService{}
+}
+
+func (us *userService) GetAll(c *gin.Context) {
+	c.JSON(http.StatusOK, utils.AppSuccess{
+		Error:   false,
+		Message: "welcome to all users",
+		Data:    repositories.GetAllUsers(),
+	})
 }
