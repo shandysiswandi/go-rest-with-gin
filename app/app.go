e7e7d941dffee3c6038df0c23e4bdc1f78d6a1e3
Shandy Siswandi <shandysiswandi@gmail.com>
2020-11-24 01:50:40 +0700

    change run app to this file

--- app/app.go
+++ app/app.go
@@ -1,8 +1,20 @@
 package app
 
+import (
+	"log"
+	"os"
+)
+
 // StartApp is
 func StartApp() {
+	// setting environment
 	env()
 
-	routes()
+	// routing
+	r := routes()
+
+	// run app
+	if err := r.Run(":" + os.Getenv("APP_PORT")); err != nil {
+		log.Fatal(err.Error())
+	}
 }
