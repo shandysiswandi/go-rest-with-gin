16df6862e3e81f4d7856c36115165c59aabccd6f
Shandy Siswandi <shandysiswandi@gmail.com>
2020-11-24 15:28:09 +0700

    fix user repo

--- repositories/user.go
+++ repositories/user.go
@@ -1,25 +1,15 @@
 package repositories
 
 import (
+	"github.com/shandysiswandi/go-rest-with-gin/config"
 	"github.com/shandysiswandi/go-rest-with-gin/models"
 )
 
 // GetAllUsers is
-func GetAllUsers() []models.User {
-	users := []models.User{
-		models.User{
-			ID:       "1",
-			Name:     "name 1",
-			Email:    "email1@email.com",
-			Password: "password1",
-		},
-		models.User{
-			ID:       "2",
-			Name:     "name 2",
-			Email:    "email2@email.com",
-			Password: "password2",
-		},
+func GetAllUsers(mu *[]models.User) error {
+	if err := config.DB.Find(mu).Error; err != nil {
+		return err
 	}
 
-	return users
+	return nil
 }
