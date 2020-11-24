81f6c7a88e6e75c4705a5058370a8002a08b63e1
Shandy Siswandi <shandysiswandi@gmail.com>
2020-11-24 16:31:52 +0700

    add func InitDB

--- config/db.go
+++ config/db.go
@@ -1,8 +1,25 @@
 package config
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/shandysiswandi/go-rest-with-gin/models"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 // DB as
 var DB *gorm.DB
+
+// InitDB is
+func InitDB() {
+	var errDB error
+	DB, errDB = gorm.Open(mysql.Open(os.Getenv("DB_URL")), &gorm.Config{PrepareStmt: true})
+
+	if errDB != nil {
+		fmt.Println("Status:", errDB)
+	}
+
+	DB.AutoMigrate(&models.User{})
+}
