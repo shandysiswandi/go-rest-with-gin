418c9bb70e97ffda8344b5b7583ef527f16e15ba
Shandy Siswandi <shandysiswandi@gmail.com>
2020-11-24 16:21:17 +0700

    add GetOne and implement it

--- services/user.go
+++ services/user.go
@@ -12,6 +12,10 @@ import (
 // UserService is .
 type UserService interface {
 	GetAll(c *gin.Context)
+	GetOne(c *gin.Context)
+	// CreateOne(c *gin.Context)
+	// UpdateOne(c *gin.Context)
+	// DeleteOne(c *gin.Context)
 }
 
 // userService is private struct
@@ -34,4 +38,17 @@ func (us *userService) GetAll(c *gin.Context) {
 	}
 
 	utils.Success(c, http.StatusOK, "welcome to all users", users)
+}
+
+func (us *userService) GetOne(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var user models.User
+	err := repositories.GetOneUser(&user, id)
+
+	if err != nil {
+		utils.Success(c, http.StatusNotFound, "user not found", user)
+		return
+	}
+
+	utils.Success(c, http.StatusOK, "get user by id", user)
 }
