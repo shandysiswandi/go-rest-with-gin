f08d0a8b21488dba6cd1b4a4260df17d24059419
Shandy Siswandi <shandysiswandi@gmail.com>
2020-11-24 15:27:04 +0700

    initial db update fix

--- app/app.go
+++ app/app.go
@@ -1,19 +1,31 @@
 package app
 
 import (
+	"fmt"
 	"log"
 	"os"
+
+	"github.com/shandysiswandi/go-rest-with-gin/config"
+	"github.com/shandysiswandi/go-rest-with-gin/models"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 // StartApp is
 func StartApp() {
 	// setting environment
-	env()
+	config.InitEnv()
 
 	// inital db
-	InitDB()
+	var errDB error
+	config.DB, errDB = gorm.Open(mysql.Open(os.Getenv("DB_URL")), &gorm.Config{})
+	if errDB != nil {
+		fmt.Println("Status:", errDB)
+	}
+	config.DB.AutoMigrate(&models.User{})
 
-	// routing
+	// initial routing
 	r := routes()
 
 	// run app
