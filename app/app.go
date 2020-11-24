dd6abff5ac05fcdc32600c1ae428118237827e36
Shandy Siswandi <shandysiswandi@gmail.com>
2020-11-24 16:32:00 +0700

    call InitDB

--- app/app.go
+++ app/app.go
@@ -1,15 +1,10 @@
 package app
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/shandysiswandi/go-rest-with-gin/config"
-	"github.com/shandysiswandi/go-rest-with-gin/models"
-
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 // StartApp is
@@ -18,12 +13,7 @@ func StartApp() {
 	config.InitEnv()
 
 	// inital db
-	var errDB error
-	config.DB, errDB = gorm.Open(mysql.Open(os.Getenv("DB_URL")), &gorm.Config{})
-	if errDB != nil {
-		fmt.Println("Status:", errDB)
-	}
-	config.DB.AutoMigrate(&models.User{})
+	config.InitDB()
 
 	// initial routing
 	r := routes()
