257b5b63d9c2ef875243ff7ee3f25e530df057d5
Shandy Siswandi <shandysiswandi@gmail.com>
2020-11-24 15:28:46 +0700

    add method for response success

--- utils/success.go
+++ utils/success.go
@@ -1,8 +1,20 @@
 package utils
 
-// AppSuccess is ...
-type AppSuccess struct {
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type appSuccess struct {
 	Error   bool        `json:"error"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
+}
+
+// Success is
+func Success(c *gin.Context, s int, m string, d interface{}) {
+	c.JSON(s, appSuccess{
+		Error:   false,
+		Message: m,
+		Data:    d,
+	})
 }
